6f06d0fb377f5e8b173818e62f84b2ccfaa2f89b
viraj-v1112 <53477506+viraj-v1112@users.noreply.github.com>
2020-12-03 20:24:49 +0530

    restructure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className='App'>Oculus 2021</div>;
-}
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './layout/Landing';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={Landing} />
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
