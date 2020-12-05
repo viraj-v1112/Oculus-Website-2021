89caa35b1bf7668d8b97a5e598632c7eea819288
Chaitya Shah <chaityashah22@gmail.com>
2020-12-05 21:40:12 +0530

    Fixed footer

--- src/shared/CustomButton.js
+++ src/shared/CustomButton.js
@@ -3,24 +3,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Button = styled.button`
-  background: linear-gradient(#ed00cc, #3f2669, #3700af, #00cdef);
-  border-radius: 2rem;
-  border: none;
-  color: #fff;
-  font-family: Montserrat;
-  font-weight: bold;
-  font-size: 1.25rem;
-  padding: 0.5rem 1.25rem;
+	background: linear-gradient(to right, #ed00cc, #3f2669, #3700af, #00cdef);
+	border-radius: 2rem;
+	border: none;
+	color: #fff;
+	font-family: Montserrat;
+	font-weight: bold;
+	font-size: 1.25rem;
+	padding: 0.5rem 1.25rem;
 `;
 
 const CustomButton = ({ buttonText, link, classname }) => {
-  return (
-    <Button className={classname}>
-      <Link to={link} style={{ textDecoration: 'none', color: '#fff' }}>
-        {buttonText}
-      </Link>
-    </Button>
-  );
+	return (
+		<Button className={classname}>
+			<Link to={link} style={{ textDecoration: 'none', color: '#fff' }}>
+				{buttonText}
+			</Link>
+		</Button>
+	);
 };
 
 export default CustomButton;
