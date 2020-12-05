84f7e0459cc8a65c61c2da256ee00d29a1e6e23e
viraj-v1112 <53477506+viraj-v1112@users.noreply.github.com>
2020-12-05 21:47:10 +0530

    Misc

--- src/components/AppDownload/AppDownload.js
+++ src/components/AppDownload/AppDownload.js
@@ -10,7 +10,7 @@ const AppDownload = () => {
       <div className='container'>
         <div className='row p-5'>
           <div className='col col-lg-6 col-md-6 col-sm-12 col- 12'>
-            <div id='logo'></div>
+            <div className='app-logo'></div>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
@@ -24,11 +24,7 @@ const AppDownload = () => {
             />
           </div>
           <div className='col col-lg-6 col-md-6 col-sm-12 col- 12 mt-5'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries,
+            <div className='app-image'></div>
           </div>
         </div>
       </div>
