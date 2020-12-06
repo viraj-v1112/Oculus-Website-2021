d976b27d865ef9dc148165eba6e5dc377859a0c2
viraj-v1112 <53477506+viraj-v1112@users.noreply.github.com>
2020-12-06 10:11:34 +0530

    Misc

--- src/layout/Landing.js
+++ src/layout/Landing.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import AppDownload from '../components/AppDownload/AppDownload';
 import Footer from '../components/Footer/Footer';
+// import MainHeader from '../components/Header/MainHeader';
 
 const Landing = () => {
   return (
     <Fragment>
+      {/* <MainHeader /> */}
       <AppDownload />
       <Footer />
     </Fragment>
