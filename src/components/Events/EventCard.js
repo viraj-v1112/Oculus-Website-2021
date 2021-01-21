d17ce31ee9c8f39a62ce3e876b8fc04c8dd535b9
viraj-v1112 <53477506+viraj-v1112@users.noreply.github.com>
2021-01-21 18:51:31 +0530

    Update

--- src/components/Events/EventCard.js
+++ src/components/Events/EventCard.js
@@ -1,93 +1,114 @@
 import React, { Fragment } from 'react';
 import { useState, useContext } from 'react';
-import CustomButton from '../../shared/CustomButton';
-// import SignIn from '../../shared/onClick/SignIn';
+import { GoVerified } from 'react-icons/go';
 import AuthContext from '../../context/AuthContext/AuthContext';
-// import Update from '../../shared/onClick/Update';
 import './EventCard.css';
 
 const EventCard = ({ event, handleOpen }) => {
-	const [ active, setActive ] = useState(false);
-	const { description, category, eventName, imageURL, likes, prizes, teamSizeAndFees } = event;
-	const { user, userData, Update, SignIn } = useContext(AuthContext);
+  const [active, setActive] = useState(false);
+  const {
+    description,
+    category,
+    eventName,
+    imageURL,
+    likes,
+    prizes,
+    teamSizeAndFees,
+  } = event;
+  const { user, userData, Update, SignIn } = useContext(AuthContext);
 
-	const handleClick = async () => {
-		if (user) {
-			await Update(user, eventName);
-		} else {
-			await SignIn(handleOpen, eventName);
-		}
-	};
+  const handleClick = async () => {
+    if (user) {
+      await Update(user, eventName);
+    } else {
+      await SignIn(handleOpen, eventName);
+    }
+  };
 
-	const isShownInterest = () => {
-		if (userData) {
-			const { Events } = userData;
-			const eventNames = Object.keys(Events);
+  const isShownInterest = () => {
+    if (userData) {
+      const { Events } = userData;
+      const eventNames = Object.keys(Events);
 
-			const val = eventNames.indexOf(eventName);
+      const val = eventNames.indexOf(eventName);
 
-			if (val === -1) return false;
-			else return true;
-		}
-	};
+      if (val === -1) return false;
+      else return true;
+    }
+  };
 
-	return (
-		<div
-			className={`booking-card ${active ? 'ACTIVE' : ''}`}
-			style={{
-				backgroundImage: `url(${imageURL})`
-			}}
-			onClick={() => setActive(!active)}
-		>
-			<div className='book-container'>
-				<div className='content'>
-					<button className='btn' disabled={isShownInterest()} onClick={handleClick}>
-						{isShownInterest() ? 'Request Recieved' : 'Interested'}
-					</button>
-				</div>
-			</div>
-			<div className='informations-container'>
-				<h2 className='title'>{eventName}</h2>
-				<div className='d-flex justify-content-between' style={{ margin: '0 10px' }}>
-					<span>{category}</span>
-					<span>{likes}</span>
-				</div>
+  return (
+    <div
+      className={`booking-card ${active ? 'ACTIVE' : ''}`}
+      style={{
+        backgroundImage: `url(${imageURL})`,
+      }}
+      onClick={() => setActive(!active)}
+    >
+      <div className='book-container'>
+        <div className='content'>
+          <button
+            className='btn'
+            disabled={isShownInterest()}
+            onClick={handleClick}
+          >
+            {isShownInterest() ? 'Request Recieved' : 'Interested'}
+          </button>
+        </div>
+      </div>
+      <div className='informations-container'>
+        <h2 className='title'>
+          {eventName}{' '}
+          {isShownInterest() && (
+            <GoVerified color='green' className='ml-1 pt-1' />
+          )}
+        </h2>
 
-				<div className='more-information'>
-					<div className='info-and-date-container'>
-						<div className='box info'>
-							<p>Prizes</p>
-							<p style={{ fontSize: '0.85rem' }}>₹ {prizes}</p>
-						</div>
-						<div className='box date'>
-							<p>Registration</p>
-							<div style={{ fontSize: '0.85rem' }}>
-								{teamSizeAndFees.map((arr, index) => {
-									return (
-										<div key={index}>
-											<span>{arr[0]} </span>
-											{arr[2] ? (
-												<Fragment>
-													<span>({arr[1]})</span> <span>: ₹ {arr[2]}</span>
-												</Fragment>
-											) : (
-												<span>: ₹ {arr[1]}</span>
-											)}
-										</div>
-									);
-								})}
-							</div>
-						</div>
-					</div>
-					<p className='disclaimer justify-para'>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eveniet perferendis culpa.
-						Expedita architecto nesciunt, rem distinctioLorem ipsum dolor sit amet consectetur, adipisicing
-						elit. Quasi eveniet perferendis culpa. Expedita architecto nesciunt, rem distinctio
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+        <div
+          className='d-flex justify-content-between'
+          style={{ margin: '0 10px' }}
+        >
+          <span>{category}</span>
+          <span>&#10084; {likes}</span>
+        </div>
+
+        <div className='more-information'>
+          <div className='info-and-date-container'>
+            <div className='box info'>
+              <p>Prizes</p>
+              <p style={{ fontSize: '0.85rem' }}>₹ {prizes}</p>
+            </div>
+            <div className='box date'>
+              <p>Registration</p>
+              <div style={{ fontSize: '0.85rem' }}>
+                {teamSizeAndFees.map((arr, index) => {
+                  return (
+                    <div key={index}>
+                      <span>{arr[0]} </span>
+                      {arr[2] ? (
+                        <Fragment>
+                          <span>({arr[1]})</span> <span>: ₹ {arr[2]}</span>
+                        </Fragment>
+                      ) : (
+                        <span>: ₹ {arr[1]}</span>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <p className='disclaimer justify-para'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+            eveniet perferendis culpa. Expedita architecto nesciunt, rem
+            distinctioLorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quasi eveniet perferendis culpa. Expedita architecto nesciunt, rem
+            distinctio
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EventCard;
--- src/context/AuthContext/AuthState.js
+++ src/context/AuthContext/AuthState.js
@@ -4,218 +4,230 @@ import AuthContext from './AuthContext';
 import { useToasts } from 'react-toast-notifications';
 
 const AuthState = ({ children }) => {
-	const [ user, setUser ] = useState(null);
-	const [ userData, setUserData ] = useState(null);
-	const { addToast } = useToasts();
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const { addToast } = useToasts();
 
-	useEffect(() => {
-		auth.onAuthStateChanged((user) => {
-			if (user) {
-				const { displayName, email, uid } = user;
-				setUser({
-					displayName,
-					email,
-					uid
-				});
-				db.collection('Users').doc(user.email).get().then((doc) => {
-					if (doc.exists) {
-						setUserData(doc.data());
-					}
-				});
-			} else {
-				setUser(null);
-				setUserData(null);
-			}
-		});
-	}, []);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, email, uid } = user;
+        setUser({
+          displayName,
+          email,
+          uid,
+        });
+        db.collection('Users')
+          .doc(user.email)
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              setUserData(doc.data());
+            }
+          });
+      } else {
+        setUser(null);
+        setUserData(null);
+      }
+    });
+  }, []);
 
-	const getUser = () => {
-		// console.log('Get user called');
-		if (user) {
-			db.collection('Users').doc(user.email).get().then((doc) => {
-				if (doc.exists) {
-					setUserData(doc.data());
-				}
-			});
-		}
-	};
+  const getUser = async () => {
+    // console.log('Get user called');
+    if (user) {
+      await db
+        .collection('Users')
+        .doc(user.email)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            setUserData(doc.data());
+          }
+        })
+  };
 
-	const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-	const SignIn = async (handleOpen, eventName) => {
-		await auth
-			.signInWithPopup(googleProvider)
-			.then((res) => {
-				var docRef = db.collection('Users').doc(res.user.email);
-				// console.log();
-				docRef
-					.get()
-					.then(function(doc) {
-						if (!doc.exists) {
-							handleOpen();
-						} else {
-							Notification('Successfully Signed In', 'success');
-							const { Events } = doc.data();
-							const eventNames = Object.keys(Events);
+  const SignIn = async (handleOpen, eventName) => {
+    await auth
+      .signInWithPopup(googleProvider)
+      .then((res) => {
+        var docRef = db.collection('Users').doc(res.user.email);
+        // console.log();
+        docRef
+          .get()
+          .then(function (doc) {
+            if (!doc.exists) {
+              handleOpen();
+            } else {
+              Notification('Successfully Signed In', 'success');
+              const { Events } = doc.data();
+              const eventNames = Object.keys(Events);
 
-							const val = eventNames.indexOf(eventName);
+              const val = eventNames.indexOf(eventName);
 
-							if (val === -1) Update(res.user, eventName);
-							else Notification('Already shown Interest!', 'warning');
-						}
-					})
-					.catch(function(error) {
-						// console.log('Error getting document:', error);
-					});
-			})
-			.catch((error) => {
-				// console.log(error.message);
-			});
-	};
+              if (val === -1) Update(res.user, eventName);
+               else Notification('Already shown Interest!', 'warning');
+            }
+          })
+          .catch(function (error) {
+            // console.log('Error getting document:', error);
+          });
+      })
+      .catch((error) => {
+        // console.log(error.message);
+      });
+  };
 
-	const Register = (formData, user, event) => {
-		// Add a new document in collection "cities"
-		db
-			.collection('Users')
-			.doc(user.email)
-			.set({
-				Events: {
-					[event]: {
-						date: '',
-						invoice_number: '',
-						fees: '',
-						registered: false,
-						slotTime: ''
-					}
-				},
-				collegeName: formData.college,
-				emailID: user.email,
-				name: user.displayName,
-				number: formData.number,
-				uid: user.uid
-			})
-			.then(function() {
-				// console.log('Document successfully written!');
-				Notification('Successfully Registered.', 'success');
-				Notification('Our Organizers will contact you shortly!', 'info');
-				getUser();
-			})
-			.catch(function(error) {
-				// console.error('Error writing document: ', error);
-			});
+  const Register = (formData, user, event) => {
+    // Add a new document in collection "cities"
+    db.collection('Users')
+      .doc(user.email)
+      .set({
+        Events: {
+          [event]: {
+            date: '',
+            invoice_number: '',
+            fees: '',
+            registered: false,
+            slotTime: '',
+          },
+        },
+        collegeName: formData.college,
+        emailID: user.email,
+        name: user.displayName,
+        number: formData.number,
+        uid: user.uid,
+      })
+      .then(function () {
+        // console.log('Document successfully written!');
+        Notification('Successfully Registered.', 'success');
+        Notification('Our Organizers will contact you shortly!', 'info');
+        getUser();
+      })
+      .catch(function (error) {
+        // console.error('Error writing document: ', error);
+      });
 
-		db
-			.collection(event)
-			.doc(user.email)
-			.set({
-				Invoice: false,
-				fees: '',
-				slotTime: '',
-				collegeName: formData.college,
-				emailID: user.email,
-				name: user.displayName,
-				number: formData.number,
-				registered: false,
-				date: '',
-				invoice_number: ''
-			})
-			.then(function() {
-				// console.log('Document successfully written!');
-			})
-			.catch(function(error) {
-				// console.error('Error writing document: ', error);
-			});
-	};
+    db.collection(event)
+      .doc(user.email)
+      .set({
+        Invoice: false,
+        fees: '',
+        slotTime: '',
+        collegeName: formData.college,
+        emailID: user.email,
+        name: user.displayName,
+        number: formData.number,
+        registered: false,
+        date: '',
+        invoice_number: '',
+      })
+      .then(function () {
+        // console.log('Document successfully written!');
+      })
+      .catch(function (error) {
+        // console.error('Error writing document: ', error);
+      });
+  };
 
-	const Update = (user, event) => {
-		var docRef = db.collection('Users').doc(user.email);
+  const Update = async (user, event) => {
+    var docRef = db.collection('Users').doc(user.email);
 
-		var data = {};
-		var userDoc;
-		docRef
-			.get()
-			.then(function(doc) {
-				if (doc.exists) {
-					userDoc = doc.data();
-					db
-						.collection(event)
-						.doc(user.email)
-						.set({
-							Invoice: false,
-							fees: '',
-							slotTime: '',
-							collegeName: userDoc.collegeName,
-							emailID: userDoc.emailID,
-							name: userDoc.name,
-							number: userDoc.number,
-							registered: false,
-							date: '',
-							invoice_number: ''
-						})
-						.then(function() {
-							// console.log('Document successfully written!');
-						})
-						.catch(function(error) {
-							// console.error('Error writing document: ', error);
-						});
-					var eventUpdate = {};
-					eventUpdate[`Events.${event}.date`] = '';
-					eventUpdate[`Events.${event}.invoice_number`] = '';
-					eventUpdate[`Events.${event}.fees`] = '';
-					eventUpdate[`Events.${event}.registered`] = false;
-					eventUpdate[`Events.${event}.slotTime`] = '';
+    var data = {};
+    var userDoc;
+    await docRef
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
+          userDoc = doc.data();
+          db.collection(event)
+            .doc(user.email)
+            .set({
+              Invoice: false,
+              fees: '',
+              slotTime: '',
+              collegeName: userDoc.collegeName,
+              emailID: userDoc.emailID,
+              name: userDoc.name,
+              number: userDoc.number,
+              registered: false,
+              date: '',
+              invoice_number: '',
+            })
+            .then(function () {
+              // console.log('Document successfully written!');
+            })
+            .catch(function (error) {
+              // console.error('Error writing document: ', error);
+            });
+          var eventUpdate = {};
+          eventUpdate[`Events.${event}.date`] = '';
+          eventUpdate[`Events.${event}.invoice_number`] = '';
+          eventUpdate[`Events.${event}.fees`] = '';
+          eventUpdate[`Events.${event}.registered`] = false;
+          eventUpdate[`Events.${event}.slotTime`] = '';
 
-					docRef
-						.update(eventUpdate)
-						.then(function() {
-							// console.log('Document successfully updated!');
-							db.collection('Users').doc(user.email).get().then((doc) => {
-								if (doc.exists) {
-									data = doc.data();
-									getUser();
-									// console.log(data);
-								}
-							});
-							Notification('Our Organizers will contact you shortly!', 'info');
-						})
-						.catch(function(error) {
-							// The document probably doesn't exist.
-							// console.error('Error updating document: ', error);
-						});
-				} else {
-					// console.log('Doc doesnt exist');
-					Logout();
-				}
-			})
-			.catch(function(error) {
-				// console.log('Error getting document:', error);
-			});
-	};
+          docRef
+            .update(eventUpdate)
+            .then(function () {
+              // console.log('Document successfully updated!');
+              db.collection('Users')
+                .doc(user.email)
+                .get()
+                .then((doc) => {
+                  if (doc.exists) {
+                    data = doc.data();
+                    // getUser('update');
+                    setUserData(data);
+                    console.log(data);
+                  }
+                });
+              Notification('Our Organizers will contact you shortly!', 'info');
+              // setTimeout(() => {
+              //   getUser('up1');
+              // }, 5000);
+            })
+            .catch(function (error) {
+              // The document probably doesn't exist.
+              // console.error('Error updating document: ', error);
+            });
+        } else {
+          // console.log('Doc doesnt exist');
+          Logout();
+        }
+      })
+      .catch(function (error) {
+        // console.log('Error getting document:', error);
+      });
+  };
 
-	const Logout = () => {
-		auth
-			.signOut()
-			.then(() => {
-				// console.log('logged out');
-				Notification('Successfully Logged Out', 'success');
-			})
-			.catch((error) => {
-				// console.log(error.message);
-			});
-	};
+  const Logout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        // console.log('logged out');
+        Notification('Successfully Logged Out', 'success');
+      })
+      .catch((error) => {
+        // console.log(error.message);
+      });
+  };
 
-	const Notification = (toastMessage, toastType) => {
-		addToast(toastMessage, {
-			appearance: toastType,
-			autoDismiss: true
-		});
-	};
+  const Notification = (toastMessage, toastType) => {
+    addToast(toastMessage, {
+      appearance: toastType,
+      autoDismiss: true,
+    });
+  };
 
-	return (
-		<AuthContext.Provider value={{ user, userData, getUser, SignIn, Register, Update, Logout }}>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{ user, userData, getUser, SignIn, Register, Update, Logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthState;
