f883e1f4a21fb50b91c53bd48013d898317850fd
viraj-v1112 <53477506+viraj-v1112@users.noreply.github.com>
2021-01-22 18:44:57 +0530

    Updates

--- src/components/AppDownload/AppDownload.js
+++ src/components/AppDownload/AppDownload.js
@@ -5,61 +5,64 @@ import Mobile from '../../assets/LandingSections/MobileMockup.png';
 import AppLogo from '../../assets/LandingSections/AppTitle.png';
 
 const AppDownload = () => {
-	return (
-		<div className='back-app'>
-			<div className='container laptop-app'>
-				<div className='row py-5 px-0'>
-					<div
-						className='col col-lg-6 col-md-6 col-sm-12 col-12 align-self-center'
-						data-aos='fade-right'
-						data-aos-duration='1000'
-					>
-						<img
-							src={AppLogo}
-							alt='mobile-app'
-							className='app-logo mb-5'
-							data-aos='zoom-in'
-							data-aos-duration='1000'
-						/>
-						<p className='justify-para'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-							been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-							galley of type and scrambled it to make a type specimen book.
-						</p>
-						<CustomButton
-							classname='button-center mt-4'
-							buttonText='Download Now'
-							link='/'
-							animation='zoom-in'
-							gradient
-						/>
-					</div>
-					<div
-						className='col col-lg-6 col-md-6 col-sm-12 col-12 '
-						data-aos='fade-left'
-						data-aos-duration='1000'
-					>
-						<img src={Mobile} alt='mobile-app' className='app-image' />
-					</div>
-				</div>
-			</div>
+  return (
+    <div className='back-app'>
+      <div className='container laptop-app'>
+        <div className='row py-5 px-0'>
+          <div
+            className='col col-lg-6 col-md-6 col-sm-12 col-12 align-self-center'
+            data-aos='fade-right'
+            data-aos-duration='1000'
+          >
+            <img
+              src={AppLogo}
+              alt='mobile-app'
+              className='app-logo mb-5'
+              data-aos='zoom-in'
+              data-aos-duration='1000'
+            />
+            <p className='justify-para'>
+              One stop for all the latest updates and bookings of your favourite
+              events at Oculus 2021.
+            </p>
+            <CustomButton
+              classname='button-center mt-4'
+              buttonText='Download Now'
+              link='/'
+              animation='zoom-in'
+              gradient
+            />
+          </div>
+          <div
+            className='col col-lg-6 col-md-6 col-sm-12 col-12 '
+            data-aos='fade-left'
+            data-aos-duration='1000'
+          >
+            <img src={Mobile} alt='mobile-app' className='app-image' />
+          </div>
+        </div>
+      </div>
 
-			<div className='container mobile-app'>
-				<div className='row px-0'>
-					<img src={AppLogo} alt='mobile-app' className='app-logo' />
-					<img src={Mobile} alt='mobile-app' className='app-image' />
-				</div>
-				<div className='row px-5 py-3'>
-					<p className='justify-para text'>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-						the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-						of type and scrambled it to make a type specimen book.
-					</p>
-				</div>
-				<CustomButton classname='button-center mt-2' buttonText='Download Now' link='/' gradient />
-			</div>
-		</div>
-	);
+      <div className='container mobile-app'>
+        <div className='row px-0'>
+          <img src={AppLogo} alt='mobile-app' className='app-logo' />
+          <img src={Mobile} alt='mobile-app' className='app-image' />
+        </div>
+        <div className='row px-5 py-3'>
+          <p className='justify-para text'>
+            One stop for all the latest updates and bookings of your favourite
+            events at Oculus 2021.
+          </p>
+        </div>
+        <CustomButton
+          classname='button-center mt-2'
+          buttonText='Download Now'
+          link='/'
+          gradient
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AppDownload;
--- src/layout/Events.js
+++ src/layout/Events.js
@@ -20,12 +20,12 @@ const Events = () => {
 
   return (
     <div className='back-app py-5' style={{ borderRadius: '0 1rem 0 0' }}>
-      <div className='container-flex py-5 mx-5'>
+      <div className='container-flex py-5 mx-5 desk'>
         <div className='row py-0'>
           <img src={EventLogo} alt='Our Events' className='event-logo-big' />
         </div>
 
-        <div className='row mt-5 laptop-nav'>
+        <div className='row mt-5 laptop-nav '>
           <div className='col-1' />
           {Category_List.map((obj, index) => (
             <div
