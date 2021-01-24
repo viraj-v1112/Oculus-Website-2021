3f3c4617ca354e4c27899425e763774828b641ae
Chaitya Shah <chaityashah22@gmail.com>
2021-01-24 19:03:36 +0530

    GitLab Commit

--- src/components/Events/EventCard.js
+++ src/components/Events/EventCard.js
@@ -5,113 +5,95 @@ import AuthContext from '../../context/AuthContext/AuthContext';
 import './EventCard.css';
 
 const EventCard = forwardRef(({ event, handleOpen }, ref) => {
-  const [active, setActive] = useState(false);
+	const [ active, setActive ] = useState(false);
 
-  useImperativeHandle(ref, () => ({
-    SET_ACTIVE() {
-      setActive(false);
-    },
-  }));
-  const {
-    description,
-    category,
-    eventName,
-    imageURL,
-    prizes,
-    teamSizeAndFees,
-    date,
-  } = event;
-  const { user, userData, Update, SignIn } = useContext(AuthContext);
+	useImperativeHandle(ref, () => ({
+		SET_ACTIVE() {
+			setActive(false);
+		}
+	}));
+	const { description, category, eventName, imageURL, prizes, teamSizeAndFees, date } = event;
+	const { user, userData, Update, SignIn } = useContext(AuthContext);
 
-  const handleClick = async () => {
-    if (user) {
-      await Update(user, eventName, handleOpen);
-    } else {
-      await SignIn(handleOpen, eventName);
-    }
-  };
+	const handleClick = async () => {
+		if (user) {
+			await Update(user, eventName, handleOpen);
+		} else {
+			await SignIn(handleOpen, eventName);
+		}
+	};
 
-  const isShownInterest = () => {
-    if (userData) {
-      const { Events } = userData;
-      const eventNames = Object.keys(Events);
+	const isShownInterest = () => {
+		if (userData) {
+			const { Events } = userData;
+			const eventNames = Object.keys(Events);
 
-      const val = eventNames.indexOf(eventName);
+			const val = eventNames.indexOf(eventName);
 
-      if (val === -1) return false;
-      else return true;
-    }
-  };
+			if (val === -1) return false;
+			else return true;
+		}
+	};
 
-  return (
-    <div
-      className={`booking-card ${active ? 'ACTIVE' : ''}`}
-      style={{
-        backgroundImage: `url(${imageURL})`,
-      }}
-      onClick={() => setActive(!active)}
-    >
-      <div className='book-container'>
-        <div className='content'>
-          <button
-            className='btn'
-            disabled={isShownInterest()}
-            onClick={handleClick}
-          >
-            {isShownInterest() ? 'Request Recieved' : 'Register'}
-          </button>
-        </div>
-      </div>
-      <div className='informations-container'>
-        <h2 className='title'>
-          {eventName}{' '}
-          {isShownInterest() && (
-            <GoVerified color='green' className='ml-1 pt-1' />
-          )}
-        </h2>
+	return (
+		<div
+			className={`booking-card ${active ? 'ACTIVE' : ''}`}
+			style={{
+				backgroundImage: `url(${imageURL})`
+			}}
+			onClick={() => setActive(!active)}
+		>
+			<div className='book-container'>
+				<div className='content'>
+					<button className='btn' disabled={isShownInterest()} onClick={handleClick}>
+						{isShownInterest() ? 'Request Recieved' : 'Register'}
+					</button>
+				</div>
+			</div>
+			<div className='informations-container'>
+				<h2 className='title'>
+					{eventName} {isShownInterest() && <GoVerified color='green' className='ml-1 pt-1' />}
+				</h2>
 
-        <div
-          className='d-flex justify-content-between'
-          style={{ margin: '0 10px' }}
-        >
-          <span>{category}</span>
+				<div className='d-flex justify-content-between' style={{ margin: '0 10px' }}>
+					<span>{category}</span>
 
-          <span>
-            <i class='fa fa-calendar-o' aria-hidden='true'></i> {date}
-          </span>
-        </div>
+					<span>
+						<i className='fa fa-calendar-o' aria-hidden='true' /> {date}
+					</span>
+				</div>
 
-        <div className='more-information'>
-          <div className='info-and-date-container'>
-            <div className='box info'>
-              <p>Prizes</p>
-              <p style={{ fontSize: '0.85rem' }}>₹ {prizes}</p>
-            </div>
-            <div className='box'>
-              <p>Registration</p>
-              <div style={{ fontSize: '0.85rem' }}>
-                {teamSizeAndFees.map((arr, index) => {
-                  return (
-                    <div key={index}>
-                      <span>{arr[0]} </span>
-                      {arr[2] ? (
-                        <Fragment>
-                          <span>({arr[1]})</span> <span>: ₹ {arr[2]}</span>
-                        </Fragment>
-                      ) : (
-                        <span>: ₹ {arr[1]}</span>
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-          <p className='disclaimer justify-para'>{description}</p>
-        </div>
-      </div>
-    </div>
-  );
+				<div className='more-information'>
+					<div className='info-and-date-container'>
+						<div className='box info'>
+							<p>Prizes</p>
+							<p style={{ fontSize: '0.85rem' }}>₹ {prizes}</p>
+						</div>
+						<div className='box'>
+							<p>Registration</p>
+							<div style={{ fontSize: '0.85rem' }}>
+								{teamSizeAndFees.map((arr, index) => {
+									return (
+										<div key={index}>
+											<span>{arr[0]} </span>
+											{arr[2] ? (
+												<Fragment>
+													<span>({arr[1]})</span> <span>: ₹ {arr[2]}</span>
+												</Fragment>
+											) : (
+												<span>: ₹ {arr[1]}</span>
+											)}
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+					<p className='disclaimer justify-para'>{description}</p>
+				</div>
+			</div>
+		</div>
+	);
 });
 
 export default EventCard;
--- src/layout/Sponsor.js
+++ src/layout/Sponsor.js
@@ -5,34 +5,34 @@ import Sponsors from '../assets/OurSponsors.png';
 import { animateScroll as scroll } from 'react-scroll';
 
 const Sponsor = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice'
+		}
+	};
 
-  useEffect(() => {
-    scroll.scrollToTop();
-  }, []);
+	useEffect(() => {
+		scroll.scrollToTop();
+	}, []);
 
-  return (
-    <div className='back-app' style={{ height: '100vh' }}>
-      <div className='container-flex py-5 mx-5'>
-        <div className='row py-0'>
-          <img src={Sponsors} alt='Our Sponsors' className='event-logo-big' />
-        </div>
-        <Lottie
-          options={defaultOptions}
-          height={300}
-          width={300}
-          style={{ position: 'relative', top: '25%' }}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className='back-app' style={{ height: '100vh', borderRadius: '0 1rem 0 0' }}>
+			<div className='container-flex py-5 mx-5'>
+				<div className='row py-0'>
+					<img src={Sponsors} alt='Our Sponsors' className='event-logo-big' />
+				</div>
+				<Lottie
+					options={defaultOptions}
+					height={300}
+					width={300}
+					style={{ position: 'relative', top: '25%' }}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Sponsor;
